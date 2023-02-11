6fe2c3f26d55ca385e619dd57fbf93a24bd4e6d9
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2023-02-12 02:28:20 +0330

    add banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,84 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      15:01
-    </div>
-  )
-}
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative p-3">
+                <img
+                  src="../images/main-banner-1.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>ipad s13+ Pro.</h5>
+                  <p>From $999.00 or $41.62/mo. </p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex gap-10 flex-wrap justify-content-between align-items-center">
+                <div className="small-banner position-relative p-3">
+                  <img
+                    src="../images/catbanner-01.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>best sales</h4>
+                    <h5>Laptops Max</h5>
+                    <p>From $999.00 <br/> or $41.62/mo. </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img
+                    src="../images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>New arrival</h4>
+                    <h5>Buy IPad Air</h5>
+                    <p>From $99.00  <br/> or $41.62/mo. </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img
+                    src="../images/catbanner-03.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>best sales</h4>
+                    <h5>ipad s13+ Pro.</h5>
+                    <p>From $999.00  <br/> or $41.62/mo. </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img
+                    src="../images/catbanner-04.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>ipad s13+ Pro.</h5>
+                    <p>From $999.00 <br/>  or $41.62/mo. </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;

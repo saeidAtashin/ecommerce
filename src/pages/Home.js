697e7c48037cf033e7abaf5f5f62d4ef54964087
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2023-02-12 14:03:17 +0330

    Featured Collection added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
 import BlogCard from "../components/BlogCard";
+import ProductCard from "../components/ProductCard";
 
 
 const Home = () => {
@@ -200,6 +201,22 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="featured-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">
+                Featured Collection
+                </h3>
+            </div>
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+
+          </div>
+        </div>
+      </section>
       <section className="maque-wrapper py-5">
       <div className="container">
         <div className="row">
@@ -238,11 +255,14 @@ const Home = () => {
         </div>
       </div>
       </section>
+
       <section className="blog-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <h3 className="section-heading">Our Latest Blogs</h3>
+              <h3 className="section-heading">
+                Our Latest Blogs
+                </h3>
             </div>
            <BlogCard />
            <BlogCard />

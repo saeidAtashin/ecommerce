72d25caf122dc0e137a283ea1facdde08e0542b6
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2023-02-15 13:23:39 +0330

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,10 @@ import React, { useEffect, useRef } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import mapboxgl from "mapbox-gl";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiPhoneCall } from "react-icons/bi";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
 
 const Contact = () => {
   const mapContainer = useRef(null);
@@ -13,14 +17,14 @@ const Contact = () => {
       "pk.eyJ1IjoibmluaWdvbCIsImEiOiJjbGU0ZXFwZWgwMmF4M29tbjJlYW12cGx2In0.yYBYMJXEc95cuX16UdHGlA";
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/mapbox/streets-v12",
       center: [51.356634, 35.684183],
+      textField: "name:en",
       zoom: 12,
     });
 
     // create marker
     const marker = new mapboxgl.Marker({
-      color: "red" ,
       element: document.createElement("img"),
       anchor: "bottom",
       img: "https://www.shareicon.net/data/512x512/2015/08/12/84141_down_512x512.png",
@@ -30,10 +34,10 @@ const Contact = () => {
 
     // set marker image
     const img = marker.getElement();
-    img.src = "https://www.shareicon.net/data/512x512/2015/08/12/84141_down_512x512.png";
+    img.src =
+      "https://www.shareicon.net/data/512x512/2015/08/12/84141_down_512x512.png";
     // get marker element
 
-    
     const element = marker.getElement();
 
     // set marker size
@@ -58,8 +62,78 @@ const Contact = () => {
             <div className="col-12 d-flex align-items-center justify-content-center">
               <div
                 ref={mapContainer}
-                style={{ width: "90%", height: "60vh" }}
+                style={{ width: "100%", height: "400px" }}
               />
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title mb-4">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Send</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineHome className="fs-5" />
+                        <address className="mb-0">
+                          Hno : Somewhere in the World
+                        </address>
+                      </li>
+                      <li  className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:+98 9368165125" > +98 9368165125 </a>
+                      </li>
+                      <li  className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5" />
+                        <a href="mailto:saeid.kase.atashin@gmail.com">
+                        saeid.kase.atashin@gmail.com
+                        </a>
+                      </li>
+                      <li  className="mb-3 d-flex gap-15 align-items-center">
+                        <BsInfoCircle className="fs-5" />
+                        <p className="mb-0"> Mondey - friday  10 AM - 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>

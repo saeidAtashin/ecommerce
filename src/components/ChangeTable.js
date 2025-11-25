50d5eaad51036428036f9b610af5a4b4c996c4f5
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2025-11-25 22:25:43 +0330

    fix

--- src/components/ChangeTable.js
+++ src/components/ChangeTable.js
@@ -4,26 +4,7 @@ const ChangeTable = ({ changeLog }) => {
   return (
     <div>
      
-      <table>
-        <thead>
-          <tr>
-            <th>Component</th>
-            <th>Prop Name</th>
-            <th>Previous Value</th>
-            <th>Next Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          {changeLog?.map((change, index) => (
-            <tr key={index}>
-              <td>{change.componentName}</td>
-              <td>{change.propName}</td>
-              <td>{JSON.stringify(change.prevValue)}</td>
-              <td>{JSON.stringify(change.nextValue)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
     </div>
   );
 };
